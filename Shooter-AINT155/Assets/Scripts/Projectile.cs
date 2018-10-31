bc90887aa7c97bec353c201e00024ad008f814fb
Jangosan <43547237+Jangosan@users.noreply.github.com>
2018-10-31 12:00:32 +0000

    AINT155 31/10/2018

---
+++ Shooter-AINT155/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float moveSpeed = 100.0f;
+    public int damage = 1;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);		
+	}
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        target.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    
+}
---
+++ Shooter-AINT155/Assets/Scripts/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireTime = 0.5f;
+
+    private bool isFiring = false;
+
+    private void setFiring()
+    {
+        isFiring = false;
+    }
+
+    private void fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        //if the game object has an audio source, it will play it when this method is executed
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        //invokes the setFiring method after the specified firing time
+        Invoke("setFiring", fireTime);
+    }
+
+	
+
+	void Update () {
+
+        //if lmb is pressed and the gun is not already firing, the weapon is fired
+		if( Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                fire();
+            }
+        }
+	}
+}
